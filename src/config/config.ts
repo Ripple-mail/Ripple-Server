f030543423d7e8478efaef8fe91578f2f0ee58cd
ultraslayyy <ultra@ultraslayyy.xyz>
2025-05-25 00:39:46 +1000

    idk man

--- src/config/config.ts
+++
@@ -1,5 +0,0 @@
-import config from 'config';
-
-export const PORT: number = config.get<number>('port');
-export const MAILDOMAINPREFIX: string = config.get<string>('mailDomainPrefix');
-export const HOST: string = config.get<string>('host');
