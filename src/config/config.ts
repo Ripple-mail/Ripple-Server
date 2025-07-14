9d2938a2c40fb5a62741d2e9b4ff19617e9e0473
ultraslayyy <ultra@ultraslayyy.xyz>
2025-07-15 02:17:56 +1000

    Delete files, ready for Go-rewrite

--- src/config/config.ts
+++
@@ -1,5 +0,0 @@
-import config from 'config';
-
-export const PORT: number = config.get<number>('port');
-export const HOST: string = config.get<string>('host');
-export const MAILDOMAINPREFIX: string = config.get<string>('mailDomainPrefix');
