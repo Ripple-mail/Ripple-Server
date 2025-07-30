9398d0b1714f52b59cf7eada427cabe30f38d613
ultraslayyy <ultra@ultraslayyy.xyz>
2025-07-31 09:10:39 +1000

    Reworked config structure

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,7 @@
 import config from 'config';
 
-export const PORT: number = config.get<number>('port');
-export const HOST: string = config.get<string>('host');
+console.log('LOADING CONFIG...');
+export const API_PORT: number = config.get<number>('api.port');
+export const SMTP_PORT: number = config.get<number>('smtp.port');
 export const MAILDOMAINPREFIX: string = config.get<string>('mailDomainPrefix');
+console.log('CONFIG LOADED');
