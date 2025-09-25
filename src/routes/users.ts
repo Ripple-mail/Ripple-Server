6c2e606206d9469a1cf2f1cba0d0d7f6885b5f52
ultraslayyy <ultra@ultraslayyy.xyz>
2025-09-25 17:47:27 +1000

    Added route to fetch logged in user's info

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,3 +1,25 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '../middleware/auth';
 import { db } from '$db/db';
+import { users } from '$db/schema';
+import { and, eq, isNull } from 'drizzle-orm';
+
+const router: Router = express.Router();
+
+router.get('/self', authMiddleware, async (req, res) => {
+    try {
+        const user = await db.query.users.findFirst({
+            where: and(
+                eq(users.id, req.user.id),
+                isNull(users.deletedAt)
+            )
+        });
+
+        return res.json({ status: 'success', user });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ status: 'error', error: 'Internal Server Error' });
+    }
+});
+
+export default router;
